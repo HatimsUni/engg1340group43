c6bc5378d611111b865c939b3dcb6b14434b7657
Gaudi <gaudihan@connect.hku.hk>
2023-05-01 20:13:21 +0800

    added isShuffle parameter to newDeck() and added drawCard(deck, n) function

--- cardingo/src/card_dealer.cpp
+++ cardingo/src/card_dealer.cpp
@@ -65,7 +65,7 @@ struct Card {
 void printVector(vector<Card> vec) { // debug purposes
     vector<Card>::iterator itr = vec.begin();
     
-    cout << "Deck: ";
+    cout << "Card: ";
     for (int i = 0; i < vec.size(); i++) {
         cout << itr->suit << itr->value << " ";
         itr++;
@@ -73,22 +73,49 @@ void printVector(vector<Card> vec) { // debug purposes
     cout << endl;
 }
 
-vector<Card> newDeck() { // return a shuffled 52-Card vector
+vector<Card> newDeck(bool isShuffle = true) { // return a shuffled 52-Card vector
     vector<Card> deck;
-    for (int i = 1; i <= 52; i++) {
+    for (int i = 1; i <= 52; i++) { // create vector with 52 Card, unshuffled
         deck.push_back(Card(i));
     }
-    random_device rd;
-    mt19937 g(rd());
-    shuffle(deck.begin(), deck.end(), g);
     
+    if (isShuffle) { // shuffle the vector
+        random_device rd;
+        mt19937 g(rd());
+        shuffle(deck.begin(), deck.end(), g);
+    }
+
     return deck;
+}
+
+vector<Card> drawCard(vector<Card> &deck, int n) { // draw n Card from top of the deck and return it as vector
+    vector<Card> cards;
+    
+    vector<Card>::iterator itr = deck.begin();
+    for (int i = 0; i < n; i++) {
+        cards.push_back(*itr);
+        deck.erase(itr);
+    }
+    
+    return cards;
 }
 
 int main() { // testing purposes
     
-    vector<Card> deck = newDeck();
+    vector<Card> deck = newDeck(false);
+    cout << endl << "full unshuffled deck" << endl;
     printVector(deck);
     
+    deck = newDeck();
+    cout << endl << "full shuffled deck" << endl;
+    printVector(deck);
+
+    vector<Card> hand1 = drawCard(deck, 3);
+    cout << endl << "draw 3 cards" << endl;
+    printVector(hand1);
+    
+    cout << endl << "card left in the deck" << endl;
+    printVector(deck);
+
     return 0;
 }
