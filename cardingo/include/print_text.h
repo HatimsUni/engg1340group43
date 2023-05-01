5904ee52ccd4eb14c2c602d2c74211028e8faf73
Hatim <hatimquetta@gmail.com>
2023-05-01 21:51:36 +0800

    print_text

---
+++ cardingo/include/print_text.h
@@ -0,0 +1,12 @@
+#ifndef CARD_DEALER_H
+#define CARD_DEALER_H
+
+//functions
+void print();
+void print_suit();
+void print_main_heading();
+void print_main_ending();
+void print_game_heading();
+void print_user_details();
+
+#endif
