4248b8eb5f7bc7eac4a651510961e0e41e1eda36
Winiera Sutanto <winiera27@gmail.com>
2023-05-02 20:31:41 +0800

    fixed the print statements

--- cardingo/src/main.cpp
+++ cardingo/src/main.cpp
@@ -7,22 +7,23 @@
 #include "../include/old_maid.h"
 #include "../include/card_dealer.h"
 #include "../include/file_handler.h"
+#include "../include/print_text.h"
 using namespace std;
 
 void choose_bigtwo(){
     string reply;
     play_bigtwo();
     while (true){
-        print("Would you like to play again? (y/n)", end = "\n");
+        print("Would you like to play again? (y/n)");
         getline(cin, reply);
         if (reply == "y"){
             choose_bigtwo();
             return;
         } else if  (reply == "n"){
-            print("Thank you. Returning to the game chooser screen", end = "\n");
+            print("Thank you. Returning to the game chooser screen");
             return;
         } else {
-            print("Wrong input. Please input your answer again", end = "\n");
+            print("Wrong input. Please input your answer again");
         }
     }
 }
@@ -31,16 +32,16 @@ void choose_blackjack(){
     string reply;
     play_blackjack();
     while (true){
-        print("Would you like to play again? (y/n)", end = "\n");
+        print("Would you like to play again? (y/n)");
         getline(cin, reply);
         if (reply == "y"){
             choose_blackjack();
             return;
         } else if  (reply == "n"){
-            print("Thank you. Returning to the game chooser screen", end = "\n");
+            print("Thank you. Returning to the game chooser screen");
             return;
         } else {
-            print("Wrong input. Please input your answer again", end = "\n");
+            print("Wrong input. Please input your answer again");
         }
     }
 }
@@ -49,16 +50,16 @@ void choose_higer_lower(){
     string reply;
     play_higher_lower();
     while (true){
-        print("Would you like to play again? (y/n)", end = "\n");
+        print("Would you like to play again? (y/n)");
         getline(cin, reply);
         if (reply == "y"){
             choose_higer_lower();
             return;
         } else if  (reply == "n"){
-            print("Thank you. Returning to the game chooser screen", end = "\n");
+            print("Thank you. Returning to the game chooser screen");
             return;
         } else {
-            print("Wrong input. Please input your answer again", end = "\n");
+            print("Wrong input. Please input your answer again");
         }
     }
 }
@@ -67,16 +68,16 @@ void choose_trump(){
     string reply;
     play_trump();
     while (true){
-        print("Would you like to play again? (y/n)", end = "\n");
+        print("Would you like to play again? (y/n)");
         getline(cin, reply);
         if (reply == "y"){
             choose_trump();
             return;
         } else if  (reply == "n"){
-            print("Thank you. Returning to the game chooser screen", end = "\n");
+            print("Thank you. Returning to the game chooser screen");
             return;
         } else {
-            print("Wrong input. Please input your answer again", end = "\n");
+            print("Wrong input. Please input your answer again");
         }
     }
 }
@@ -86,16 +87,16 @@ void choose_old_maid(){
     string reply;
     play_old_maid();
     while (true){
-        print("Would you like to play again? (y/n)", end = "\n");
+        print("Would you like to play again? (y/n)");
         getline(cin, reply);
         if (reply == "y"){
             choose_old_maid();
             return;
         } else if  (reply == "n"){
-            print("Thank you. Returning to the game chooser screen", end = "\n");
+            print("Thank you. Returning to the game chooser screen");
             return;
         } else {
-            print("Wrong input. Please input your answer again", end = "\n");
+            print("Wrong input. Please input your answer again");
         }
     }
 }
@@ -104,19 +105,19 @@ void choose_game(){
     string input;
     int number;
     while(true){
-        print("We have many card games, please pick one by entering the number (0-5)", end = "\n");
-        print("1 Big Two", end = "\n");
-        print("2 Blackjack", end = "\n");
-        print("3 Higer Lower", end = "\n");
-        print("4 Trump", end = "\n");
-        print("5 Old Maid", end = "\n");
-        print("0 Exit", end = "\n");
+        print("We have many card games, please pick one by entering the number (0-5)");
+        print("1 Big Two");
+        print("2 Blackjack");
+        print("3 Higer Lower");
+        print("4 Trump");
+        print("5 Old Maid");
+        print("0 Exit");
         getline(cin, input);
         
         if (input.size() == 1 && input.find_first_not_of("012345") == string::npos){
             number = stoi(input);
             if (number == 0){
-                print("Thank you for playing", end  = "\n");
+                print("Thank you for playing");
                 return;
             } else if (number == 1){
                 print_game_heading("bigtwo");
@@ -135,7 +136,7 @@ void choose_game(){
                 choose_old_maid();
             }
         } else {
-            print("Invalid input, please reenter a number from 0-5", end="\n");
+            print("Invalid input, please reenter a number from 0-5");
         }
     }
 }
