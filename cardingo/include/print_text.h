f0f8a901c1ed742aeeec792c03a99e6a1b596fe5
Hatim <hatimquetta@gmail.com>
2023-05-02 12:15:32 +0800

    print_text.h fix

--- cardingo/include/print_text.h
+++ cardingo/include/print_text.h
@@ -1,12 +1,15 @@
 #ifndef CARD_DEALER_H
 #define CARD_DEALER_H
 
+#include <string>
+using namespace std;
+
 //functions
-void print();
-void print_suit();
+void print(string text = "", string color = "default", bool bold = false, string end = "\n");
+void print_suit(string suit);
 void print_main_heading();
 void print_main_ending();
-void print_game_heading();
-void print_user_details();
+void print_game_heading(string game);
+void print_user_details(string user, string game, int total = 0, int played = 0, int score = 0);
 
 #endif
