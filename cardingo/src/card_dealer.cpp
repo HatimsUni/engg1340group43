a4faad96f3b497d8185787f6124b73aea3bb999c
Gaudi <gaudihan@connect.hku.hk>
2023-05-03 03:21:46 +0800

    implemented string vector without depending on Card vector and Card struct, Card struct now unnecesarry and removed

--- cardingo/src/card_dealer.cpp
+++ cardingo/src/card_dealer.cpp
@@ -6,68 +6,24 @@
 #include "../include/card_dealer.h"
 using namespace std;
 
-struct Card {
-    int rank; // rank of card e.g. 4th-lowest, 17th-lowest, 39-th lowest, etc.
-    string suit;
-    string value;
+string intToCard(int inputInt) { // inputInt: [1, 52]
+    string suit, value;
+    inputInt--;
     
-    // empty card
-    Card() { 
-        rank = 0;
-        suit = "NULLsuit";
-        value = "NULLvalue";
-    };
+    if (inputInt%4 == 0) suit = "D"; // diamond
+    else if (inputInt%4 == 1) suit = "C"; // club
+    else if (inputInt%4 == 2) suit = "H"; // heart
+    else if (inputInt%4 == 3) suit = "S"; // spade
+    else suit = "invalidSuit";
     
-    // given the rank, it will determine the suit and value by itself
-    Card(int inputRank) { // inputRank: [1, 52]
-        rank = inputRank;
-        // suit = rankToSuit(inputRank);
-        // value = rankToValue(inputRank);
-        
-        inputRank--; // [0, 51]
-        if (inputRank%4 == 0) {
-            suit = "D"; // (♦)
-        } else if (inputRank%4 == 1) {
-            suit = "C"; // (♣)
-        } else if (inputRank%4 == 2) {
-            suit = "H"; // (♥)
-        } else if (inputRank%4 == 3) {
-            suit = "S"; // (♠)
-        } else {
-            suit = "invalidSuit";
-        }
-        if (inputRank/4 <= 8) { // [0, 8]
-            value =  to_string(inputRank/4 + 2);
-        } else if (inputRank/4 == 9) {
-            value = "J";
-        } else if (inputRank/4 == 10) {
-            value = "Q";
-        } else if (inputRank/4 == 11) {
-            value = "K";
-        } else if (inputRank/4 == 12) {
-            value = "A";
-        } else {
-            value = "invalidValue";
-        }
-    };
+    if (inputInt/4 <= 8) value =  to_string(inputInt/4 + 2);
+    else if (inputInt/4 == 9) value = "J";
+    else if (inputInt/4 == 10) value = "Q";
+    else if (inputInt/4 == 11) value = "K";
+    else if (inputInt/4 == 12) value = "A";
+    else value = "invalidValue";
     
-    // custom rank, suit, and value
-    Card(int inputRank, string inputSuit, string inputValue) { // inputRank: [1, 52]
-        rank = inputRank;
-        suit = inputSuit;
-        value = inputValue;
-    };
-};
-
-void printCardVector(vector<Card> vec) { // debug purposes
-    vector<Card>::iterator itr = vec.begin();
-    
-    cout << "Card: ";
-    for (int i = 0; i < vec.size(); i++) {
-        cout << itr->suit << itr->value << " ";
-        itr++;
-    }
-    cout << endl;
+    return suit + value; 
 }
 
 void printStringVector(vector<string> vec) { // debug purposes
@@ -82,9 +38,9 @@ void printStringVector(vector<string> vec) { // debug purposes
 }
 
 vector<string> newDeck(bool isShuffle) { // return a shuffled 52-Card vector
-    vector<Card> deck;
+    vector<string> deck;
     for (int i = 1; i <= 52; i++) { // create vector with 52 Card, unshuffled
-        deck.push_back(Card(i));
+        deck.push_back(intToCard(i));
     }
     
     if (isShuffle) { // shuffle the vector
@@ -92,17 +48,8 @@ vector<string> newDeck(bool isShuffle) { // return a shuffled 52-Card vector
         mt19937 g(rd());
         shuffle(deck.begin(), deck.end(), g);
     }
-    
-    vector<string> stringDeck;
-    
-    vector<Card>::iterator itr = deck.begin();
-    for (int i = 0; i < deck.size(); i++) {
-        string tempStringCard = itr->suit + itr->value;
-        stringDeck.push_back(tempStringCard);
-        itr++;
-    }
 
-    return stringDeck;
+    return deck;
 }
 
 vector<string> drawCard(vector<string> &deck, int n) { // draw n Card from top of the deck and return it as vector
