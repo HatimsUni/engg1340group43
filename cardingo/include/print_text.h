aa29983176c3f117da1e7a5830857c1fbd81363b
Hatim <hatimquetta@gmail.com>
2023-05-05 22:18:19 +0800

    added printCard in print_text

--- cardingo/include/print_text.h
+++ cardingo/include/print_text.h
@@ -12,6 +12,7 @@ void printMainHeading();
 void printMainEnding();
 void printGameHeading(string game);
 void printUserDetails(string user, string game, int total = 0, int played = 0, int score = 0);
+void printCard(string card, bool show = false);
 void printCards(vector<string> deck, int n = 5, bool show = false);
 
 #endif
