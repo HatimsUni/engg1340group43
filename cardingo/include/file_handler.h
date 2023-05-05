944875078baea86f09b9bdc51abf51bafcad0d62
Winiera Sutanto <winiera27@gmail.com>
2023-05-06 00:16:30 +0800

    fixed some bugs

--- cardingo/include/file_handler.h
+++ cardingo/include/file_handler.h
@@ -2,6 +2,8 @@
 #define FILE_HANDLER_H
 
 //functions
+#include <string>
+using namespace std;
 void printData(string username);
 void updateFile(string username, int games_played = 0, int crazyeights = 0, int blackjack = 0, int old_maid = 0, int trump = 0, int score = 0);
 
