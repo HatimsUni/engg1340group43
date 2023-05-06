28abae68e309d1317af962e8e351bd249fedc0f7
Winiera Sutanto <winiera27@gmail.com>
2023-05-06 12:49:03 +0800

    ignore this

--- cardingo/include/file_handler.h
+++ cardingo/include/file_handler.h
@@ -3,8 +3,11 @@
 
 //functions
 #include <string>
+#include <vector>
 using namespace std;
+
 void printData(string username);
 void updateFile(string username, int games_played = 0, int crazyeights = 0, int blackjack = 0, int higher_lower = 0, int old_maid = 0, int trump = 0, int score = 0);
+vector <string> getRules(string game_name);
 
 #endif
