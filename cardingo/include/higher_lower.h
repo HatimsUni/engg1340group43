03e6732d6ed12a97b470db973f6c34fa10021afa
aishani2 <aishanim@connect.hku.hk>
2023-05-06 12:46:21 +0800

    added higher lower

---
+++ cardingo/include/higher_lower.h
@@ -0,0 +1,6 @@
+#ifndef HIGHER_LOWER_H
+#define HIGHER_LOWER_H
+
+int playHigherLower();
+
+#endif
---
+++ cardingo/src/higher_lower.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "../include/higher_lower.h"
+#include "../include/card_dealer.h"
+#include "../include/print_text.h"
+#include "../include/sleep.h"
+#include "../include/score.h"
+
+int playHigherLower() {
+    return game_score;
+}
